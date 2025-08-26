30101a992cc26e7c5f9e96b33cc4fc1de18400c6
tanay1845 <tanay23186@gmail.com>
2025-08-26 19:59:30 +0530

    random

--- controllers/url.controller.js
+++ controllers/url.controller.js
@@ -36,7 +36,7 @@ const generateShortUrl = async (req, res) => {
 
         const shortId = nanoid(8);
 
-        const qrUrl = `http://localhost:3000/${shortId}`
+        const qrUrl = `https://url-shortner-frontend-bay.vercel.app/${shortId}`
 
         const qrCodeImg = await QRCode.toDataURL(qrUrl);
         
